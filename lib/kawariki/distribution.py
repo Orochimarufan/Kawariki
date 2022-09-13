3d06f078876e035a7ca4a7f0766dacafaec65d28
Taeyeon Mori <taeyeon@oro.sodimm.me>
2022-09-13 21:53:07 +0200

    core: Allow using {platform} in dist URLs

--- lib/kawariki/distribution.py
+++ lib/kawariki/distribution.py
@@ -1,12 +1,13 @@
 from functools import cached_property
 from json import load
 from pathlib import Path
-from typing import (ClassVar, Dict, List, Optional, Sequence, Tuple, Type,
-                    TypedDict, TypeVar, Union)
+from typing import (ClassVar, Dict, Generic, List, Optional, Sequence, Tuple,
+                    Type, TypedDict, TypeVar, Union)
 
 from .misc import version_str
 
 D = TypeVar("D", bound="Distribution")
+DI = TypeVar("DI", bound="DistributionInfo")
 
 
 class DistributionInfo(TypedDict, total=False):
@@ -18,8 +19,8 @@ class DistributionInfo(TypedDict, total=False):
     alias: Sequence[str]    # Aliases for humans
 
 
-class Distribution:
-    info: DistributionInfo
+class Distribution(Generic[DI]):
+    info: DI
     path: Path
     platform: str
 
@@ -27,26 +28,33 @@ class Distribution:
     strip_leading: ClassVar[Union[bool, str]] = True
     fill_platform: ClassVar[bool] = False
     any_platform: ClassVar[bool] = False
+    format_url: ClassVar[bool] = True
     default_binary_name: ClassVar[str]
 
-    def __init__(self, info: DistributionInfo, dist_path: Path, platform=None):
-        if platform is not None and self.fill_platform:
+    def __init__(self, info: DI, dist_path: Path, platform=None):
+        if "platforms" in info:
+            if platform is None:
+                self.platform = info["platforms"][0]
+            elif platform in info["platforms"]:
+                self.platform = platform
+            else:
+                raise ValueError(f"Incompatible platform set: {info['platforms']}")
+        elif platform is not None and self.fill_platform:
             self.platform = platform
-        elif "platforms" in info and (platform is None or platform in info["platforms"]):
-            self.platform = info["platforms"][0]
         elif self.any_platform:
             self.platform = "any"
         else:
             raise ValueError(f"Missing platforms key in distribution {info}")
-        
+
         if self.dist_name is None:
             self.dist_name = dist_path.name
-        
+
         self.info = info
         self.path = dist_path / self.name
 
     @property
     def version(self) -> Tuple[int, ...]:
+        # Return tuple for easy comparison with (1,4) version tuple literals
         return tuple(self.info["version"])
 
     @property
@@ -75,11 +83,14 @@ class Distribution:
     @property
     def url(self) -> str:
         if "url" in self.info:
-            return self.info["url"]
-        url = self.generate_url()
+            if not self.format_url:
+                return self.info["url"]
+            url = self.info["url"].format(platform=self.platform)
+        else:
+            url = self.generate_url()
         self.__dict__["url"] = url
         return url
-    
+
     def generate_url(self) -> str:
         raise NotImplementedError()
 
@@ -90,12 +101,12 @@ class Distribution:
         return f"<{self.__class__.__name__} {version_str(self.version)} '{self.name}' at 0x{id(self):x}>"
 
     @classmethod
-    def load(cls: Type[D], infos: Sequence[DistributionInfo], dist_path: Path, platform: Optional[str]=None) -> List[D]:
+    def load(cls: Type[D], infos: Sequence[DI], dist_path: Path, platform: Optional[str] = None) -> List[D]:
         return [cls(i, dist_path, platform)
-            for i in infos
-            if platform is None or "platforms" not in i or platform in i["platforms"]]
+                for i in infos
+                if platform is None or "platforms" not in i or platform in i["platforms"]]
 
     @classmethod
-    def load_json(cls: Type[D], filename: Path, dist_path: Path, platform: Optional[str]=None) -> List[D]:
+    def load_json(cls: Type[D], filename: Path, dist_path: Path, platform: Optional[str] = None) -> List[D]:
         with open(filename, "r") as f:
             return cls.load(load(f), dist_path, platform)
