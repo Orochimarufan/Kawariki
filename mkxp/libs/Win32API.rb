4f21d074868cfc0de8afda6ea6ce5b3faa4f822f
Taeyeon Mori <taeyeon@oro.sodimm.me>
2023-11-03 00:23:53 +0100

    mkxp/Win32API: Stub some common User32 exports

--- mkxp/libs/Win32API.rb
+++ mkxp/libs/Win32API.rb
@@ -14,12 +14,12 @@ Object.remove_const :Win32API
 
 module Win32API
     module Kernel32
-        GetPrivateProfileInt = ->(appname, keyname, default, filename) do
+        GetPrivateProfileInt = GetPrivateProfileIntA = ->(appname, keyname, default, filename) do
             Preload.require "PreloadIni.rb"
             s = Preload::Ini.readIniString filename, appname, keyname
             s.nil? ? default : s.to_i
         end
-        GetPrivateProfileString = ->(appname, keyname, default, ret, size, filename) do
+        GetPrivateProfileString = GetPrivateProfileStringA = ->(appname, keyname, default, ret, size, filename) do
             Preload.require "PreloadIni.rb"
             if appname.nil? then
                 res = Preload::Ini.readIniSections(filename).join("\0") + "\0"
@@ -39,35 +39,76 @@ module Win32API
             ret[res.size] = "\0"
             res.size
         end
-        WritePrivateProfileString = ->(appname, keyname, value, filename) do
+        WritePrivateProfileString = WritePrivateProfileStringA = ->(appname, keyname, value, filename) do
+            Preload.require "PreloadIni.rb"
             writeIniString filename, appname, keyname, value
         end
     end
 
-    Libraries = {
-        "kernel32" => {
-            "GetPrivateProfileInt" => Kernel32::GetPrivateProfileInt,
-            "GetPrivateProfileIntA" => Kernel32::GetPrivateProfileInt,
-            "GetPrivateProfileString" => Kernel32::GetPrivateProfileString,
-            "GetPrivateProfileStringA" => Kernel32::GetPrivateProfileString,
-            "WritePrivateProfileString" => Kernel32::WritePrivateProfileString,
-            "WritePrivateProfileStringA" => Kernel32::WritePrivateProfileString,
-        },
+    module User32
+        FindWindow = FindWindowA = ->(cls, wnd) do
+            return 1
+        end
+        FindWindowEx = ->(parent, ca, cls, wnd) do
+            return 1
+        end
+        GetAsyncKeyState = ->(key) do
+            # Very naive
+            return 128 if Input.pressex? key
+            return 0
+        end
+        GetClientRect = ->(hWnd, out_rect) do
+            return 0 unless hWnd == 1
+            out_rect.byteslice(0, 16, [0, 0, Graphics.width, Graphics.height].pack("llll"))
+            return 1
+        end
+        GetCursorPos = ->(out_point) do
+            out_point.byteslice(0, 8, [Input.mouse_x, Input.mouse_y].pack("ll"))
+            return 1
+        end
+        GetKeyboardLayout = ->(thread) do
+            return 0
+        end
+        GetSystemMetrics = ->(index) do
+            return 0 if index == 23 # SM_SWAPBUTTON
+            Preload.print("Warning: user32#GetSystemMetrics index #{index} not implemented")
+            return 0
+        end
+        GetWindowRect = ->(hWnd, out_rect) do
+            return 0 unless hWnd == 1
+            out_rect.byteslice(0, 16, [0, 0, Graphics.width, Graphics.height].pack("llll"))
+            return 1
+        end
+        MapVirtualKeyEx = ->(code, map, layout) do
+            return 0 unless layout == 0
+            return code
+        end
+        ScreenToClient = ->(hWnd, point) do
+            return 1 unless hWnd != 1
+            return 0
+        end
+        ShowCursor = ->(show) do
+            Graphics.show_cursor = show == 1
+            return show
+        end
+    end
 
-        "steam_api" => {
-            # TODO: Forward to native steamapi?
-            "SteamAPI_Init" => ->{1},
-            "SteamAPI_Shutdown" => ->{},
-        },
+    module SteamAPI
+        # TODO: Forward to native steamapi?
+        SteamAPI_Init = ->{1}
+        SteamAPI_Shutdown = ->{}
+    end
+
+    Libraries = {
+        "kernel32" => Kernel32,
+        "user32" => User32,
+        "steam_api" => SteamAPI,
     }
 
     def self.new(dllname, func, *rest)
         dllname = dllname[...-4] if dllname[...-4] == ".dll"
         lib = Libraries[dllname]
-        unless lib.nil? then
-            result = lib[func]
-            return result unless result.nil?
-        end
+        return lib.const_get(func, false) if lib.const_defined?(func, false) unless lib.nil?
         Preload.print("Warning: Win32API not implemented: #{dllname}##{func}")
         return ->(*args){Preload.print "(STUB) #{dllname}##{func}: #{args}"}
     end
