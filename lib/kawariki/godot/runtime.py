d5b9e6b364d32896e2a8790f1ddb7668f79a1912
Taeyeon Mori <taeyeon@oro.sodimm.me>
2024-11-23 19:48:25 +0100

    godot: Add version detection and distro selection

--- lib/kawariki/godot/runtime.py
+++ lib/kawariki/godot/runtime.py
@@ -11,6 +11,8 @@ from ..process import ProcessLaunchInfo
 from ..misc import ErrorCode, version_str
 from ..distribution import DistributionInfo, Distribution
 
+from .pack import PackReader
+
 
 GodotDistro = Distribution[DistributionInfo]
 
@@ -41,27 +43,37 @@ class Runtime(IRuntime):
     def versions(self) -> Sequence[GodotDistro]:
         return GodotDistro.load_json(self.resources / "versions.json", self.app.dist_path / "godot", self.app.platform)
 
-    def select_version(self, filter: Sequence[int] = ()) -> GodotDistro:
+    def select_version(self, version: Sequence[int] = ()) -> GodotDistro:
         # TODO: Make selectable and such
-        matching = [ ver for ver in self.versions if ver.version[:len(filter)] == filter ]
-        if not matching:
-            self.app.show_error(f"No Godot distribution available for version {version_str(filter)}")
-            raise ErrorCode(110)
-        ver = matching[0]
-        if not ver.available:
-            self.try_download_version(ver)
-        return ver
+        versions = sorted(self.versions, key=lambda dist: dist.version, reverse=True)
+        for granul in (3, 2, 1):
+            ggv = tuple(version[:granul])
+            try:
+                return next(dist for dist in versions if dist.version[:granul] == ggv)
+            except StopIteration:
+                pass
+        self.app.show_error(f"No Godot distribution available for version {version_str(version)}")
+        raise ErrorCode(110)
 
     # Run
     def run(self, game: Game, arguments: Sequence[str], *, no_overlayns=False, **kwds):
-        try:
-            dist = self.select_version()
-        except ErrorCode as e:
-            return e.code
-
         pack = game.godot_pack
         if not pack:
             raise RuntimeError("Invalid Game instance passed to Godot Runtime")
+
+        with PackReader.open(pack) as reader:
+            version = reader.engine_version
+
+        print(f"Found Godot engine version {version_str(version)}")
+
+        try:
+            dist = self.select_version(version)
+            print(f"Selected '{dist.name}' distribution")
+            if not dist.available:
+                self.try_download_version(dist)
+        except ErrorCode as e:
+            return e.code
+
         proc = ProcessLaunchInfo(self.app, [dist.binary, "--main-pack", pack.name])
         proc.workingdir = game.root
 
